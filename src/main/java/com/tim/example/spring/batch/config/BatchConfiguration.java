c5d6b6fc6b9253efc2f66b7f84420f31c8d79971
Tim Richard <tim.f.richard@gmail.com>
2021-12-30 19:00:56 -0600

    Created the database diagram

--- src/main/java/com/tim/example/spring/batch/config/BatchConfiguration.java
+++ src/main/java/com/tim/example/spring/batch/config/BatchConfiguration.java
@@ -1,10 +1,7 @@
 package com.tim.example.spring.batch.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.scope.StepScope;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
@@ -12,12 +9,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BatchConfiguration {
 
-
-    @Bean
-    @Primary
-    public StepScope stepScope() {
-        final StepScope stepScope = new StepScope();
-        stepScope.setAutoProxy(false);
-        return stepScope;
-    }
+/* Not sure if we need this leaving it here and commented out in case we have StepScope issues */
+//    @Bean
+//    @Primary
+//    public StepScope stepScope() {
+//        final StepScope stepScope = new StepScope();
+//        stepScope.setAutoProxy(false);
+//        return stepScope;
+//    }
 }
